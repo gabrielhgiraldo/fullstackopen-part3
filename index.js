c93a4d571c7d3bf9abaf46e68a22e0f3734447c6
Gabriel Giraldo-Wingler <gabrielhgiraldo@gmail.com>
2021-05-15 19:28:38 -0400

    3.5: Phonebook backend step5

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
     { 
         "name": "Arto Hellas", 
@@ -51,6 +53,14 @@ app.delete('/api/persons/:id', (request, response) => {
     persons = persons.filter(person => person.id !== id)
 
     response.status(204).end()
+})
+app.post('/api/persons', (request, response) => {
+    const person = request.body
+    person.id = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER)
+
+    persons = persons.concat(person)
+    
+    response.json(person)
 })
 
 const PORT = 3001
