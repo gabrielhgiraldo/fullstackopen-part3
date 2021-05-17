9d007bc74bf833a17ae024787e2595a7373c8cb8
Gabriel Giraldo-Wingler <gabrielhgiraldo@gmail.com>
2021-05-17 18:53:19 -0400

    3.18*: Phonebook database step6

--- index.js
+++ index.js
@@ -14,50 +14,24 @@ app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 
-let persons = [
-    { 
-        "name": "Arto Hellas", 
-        "number": "040-123456",
-        "id": 1
-    },
-    { 
-        "name": "Ada Lovelace", 
-        "number": "39-44-5323523",
-        "id": 2
-    },
-    { 
-        "name": "Dan Abramov", 
-        "number": "12-43-234345",
-        "id": 3
-    },
-    { 
-        "name": "Mary Poppendieck", 
-        "number": "39-23-6423122",
-        "id": 4
-    }
-]
-
 app.get('/info', (request, response) => {
-    response.send((
-        `
-        <p>Phonebook as has info for ${persons.length} people</p>
-        <p>${new Date()}</p>
-        `
-    ))
+    Person.find({})
+        .then(persons => {
+            response.send((
+                `
+                <p>Phonebook as has info for ${persons.length} people</p>
+                <p>${new Date()}</p>
+                `
+            ))
+    })
 })
 app.get('/api/persons', (request, response) => {
     Person.find({}).then(persons => response.json(persons))
 })
-app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-    
-    if (person){
-        response.json(person)
-    }
-    else {
-        response.status(404).end()
-    }
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+        .then(person => response.json(person))
+        .catch(error => next(error))
 })
 app.delete('/api/persons/:id', (request, response, next) => {
     Person.findByIdAndRemove(request.params.id)
@@ -89,7 +63,7 @@ app.post('/api/persons', (request, response) => {
         response.json(person)
     })
 })
-app.put('/api/persons/:id', (request, response) => {
+app.put('/api/persons/:id', (request, response, next) => {
     const body = request.body
 
     if (!body.name) {
